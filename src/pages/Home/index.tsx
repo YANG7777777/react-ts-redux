251b834756696df13bc464e8f8b2b68469f6b19e
yangjia <yangjia1416@163.com>
2026-02-11 19:45:53 +0800

    style: 首页暂无数据

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,14 +1,10 @@
-import {Button, Tooltip} from 'antd';
-import { SearchOutlined } from '@ant-design/icons';
+import { Empty} from 'antd';
 import styles from './home.module.scss'
 
 const HomePage = () => {
     return (
         <div className={styles.home}>
-            {/* <div className={styles.title}>首页</div> */}
-            <Tooltip title="search">
-                <Button type="primary" shape="circle" icon={<SearchOutlined />} />
-            </Tooltip>
+            <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} description="暂无数据" />
         </div>
     );
 };
